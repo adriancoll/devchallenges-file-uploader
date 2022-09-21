8011b7fefb382e0135141138367155d73f05b2d6
adriancoll <nege2002@gmail.com>
2022-09-21 10:35:03 +0200

    fix: drop image

--- src/components/ui/Droppable.tsx
+++ src/components/ui/Droppable.tsx
@@ -1,46 +1,51 @@
-import { DragEvent, useContext, useEffect } from "react";
+import { DragEvent, useContext, useEffect } from 'react'
 
-import mountains from "../../assets/images/image.svg";
-import { UIContext } from "../../context";
-import { useUploadImage } from "../../hooks/useUploadImage";
+import mountains from '../../assets/images/image.svg'
+import { UIContext } from '../../context'
+import { useUploadImage } from '../../hooks/useUploadImage'
 
 export const Droppable = () => {
-  const { isDragging, setIsDragging } = useContext(UIContext);
+  const { isDragging, setIsDragging } = useContext(UIContext)
 
-  const { setFile } = useUploadImage();
+  const { setFile } = useUploadImage()
 
   const handleDrop = (event: DragEvent<HTMLDivElement>) => {
-    event.stopPropagation();
-    event.preventDefault();
+    event.preventDefault()
+    event.stopPropagation()
 
-    const items = event.dataTransfer;
-    console.log(items);
+    const items = event.dataTransfer.files
+    console.log(items)
 
-    //  if (!files || files.length === 0) return;
+    if (!items || items.length === 0) return
 
-    //  setFile(files[0]);
-  };
+    setFile(items[0])
+  }
 
   useEffect(() => {
-    window.addEventListener("mouseleave", () => setIsDragging(false));
+    window.addEventListener('mouseleave', () => setIsDragging(false))
 
-    return window.removeEventListener("mouseleave", () => setIsDragging(false));
-  }, []);
+    return window.removeEventListener('mouseleave', () => setIsDragging(false))
+  }, [])
 
   return (
     <div
-      onDragOver={() => setIsDragging(true)}
-      onDragLeave={() => setIsDragging(false)}
+      onDragOver={(e) => {
+        e.preventDefault()
+        setIsDragging(true)
+      }}
+      onDragLeave={(e) => {
+        e.preventDefault()
+        setIsDragging(false)
+      }}
       onDrop={handleDrop}
-      style={{ borderWidth: "2px" }}
+      style={{ borderWidth: '2px' }}
       className={`${
-        isDragging ? "hue-rotate-180" : ""
-      } bg-drop-zone select-none px-20 py-10 gap-6 flex flex-col min-w-[338px] justify-evenly items-center rounded-xl border-dashed border-primary-light transition-all ease-in duration-300`}
-    >
-      <img src={mountains} alt="drag image here" />
-      <p className="text-primary-text select-none text-xs">
+        isDragging ? 'hue-rotate-180' : ''
+      } bg-drop-zone select-none px-20 py-10 gap-6 flex flex-col min-w-[338px] justify-evenly items-center rounded-xl border-dashed border-primary-light transition-all ease-in duration-300`}>
+      <img src={mountains} alt='drag image here' />
+      <p className='text-primary-text select-none text-xs'>
         Drag & Drop your image here
       </p>
     </div>
-  );
-};
+  )
+}
